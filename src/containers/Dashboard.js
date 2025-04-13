f32cb73b8f307ade76af17e1eff40caa82b87e26
Anthony Dupuis <votre.email@example.com>
2025-04-13 11:55:26 +0200

    dashboard.js missclic fixed

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,6 +2,7 @@ import { formatDate } from '../app/format.js'
 import DashboardFormUI from '../views/DashboardFormUI.js'
 import BigBilledIcon from '../assets/svg/big_billed.js'
 import { ROUTES_PATH } from '../constants/routes.js'
+import USERS_TEST from '../constants/usersTest.js'
 import Logout from "./Logout.js"
 
 export const filteredBills = (data, status) => {
